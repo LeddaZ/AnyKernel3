e53c757e514a4440e6b5ff5246fac6433204adcd
Leonardo Ledda <leonardoledda@gmail.com>
2023-08-26 15:54:46 +0200

    AnyKernel3: Adapt to KikiQ
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Kiki by LeddaZ
+kernel.string=KikiQ by LeddaZ
 do.devicecheck=1
 do.modules=0
 do.systemless=1
@@ -15,7 +15,7 @@ device.name2=curtana
 device.name3=excalibur
 device.name4=gram
 device.name5=joyeuse
-supported.versions=11.0-13.0
+supported.versions=10.0
 supported.patchlevels=
 '; } # end properties
 
